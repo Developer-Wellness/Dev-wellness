98bd6d21a5659ffa28577f035927ad29fa48ddcb
seanlanders92 <seanlanders92@gmail.com>
2020-03-04 14:02:42 -0800

    built login front end

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,13 @@
 'use strict';
 
-app.post('/')
+function checkUser(){
+  let username = localStorage.getItem('user');
+  if(username){
+    $('.btn btn-primary').hide();
+  }else{
+    let userNameGet = $('#userId');
+    localStorage.setItem('user', userNameGet);
+  }
+}
+
+$(window).on('load', checkUser());
